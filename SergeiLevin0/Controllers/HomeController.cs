72c8d707564e9856a9ca9c6a74955447a03b105e
olga <olga.levina.s@gmail.com>
2019-11-11 22:17:48 +0300

    1й контроллер

---
+++ SergeiLevin0/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SergeiLevin0.Controllers
+{
+    //[Controller]
+    public class HomeController : Controller //если не наследовать от класса Controller, то нужно подписать атрибутом Controller.
+    {
+        private readonly IConfiguration _Configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            //return Content("Hello, it's firts contorller");
+            return Content(_Configuration["CustomData"]);
+        }
+    }
+}
--- SergeiLevin0/Startup.cs
+++ SergeiLevin0/Startup.cs
@@ -21,6 +21,7 @@ namespace SergeiLevin0
         //набиваем сервисами, с которыми в дальнейшем будет работать наше приложение
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,10 +32,21 @@ namespace SergeiLevin0
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+
+            //app.UseMvcWithDefaultRoute(); автоматическая конфигурация того, что ниже
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync(Configuration["CustomData"]);
-            });
+                routes.MapRoute(name:"default", template:"{controller=Home}/{action=Index}/{id?}"); //?-опциональный параметр, ! - обязательный параметр (как и ничего) , если параметр обязательный, то его отсуттвие в вводе пользователя приведет к ошибке
+                // для action  - можно ничего не устанавливать, либо установить значение по умолчанию, в данном случае Index
+                // аналогично для конроллера, можно утсановить значение по умолчанию Home, например, когда сервер обращается сам к себе
+            }); // новый обработчик
+            
+            // убираем прежный обработчик, т.к. существует и работает только первый
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(Configuration["CustomData"]);
+            //});
         }
     }
 }
