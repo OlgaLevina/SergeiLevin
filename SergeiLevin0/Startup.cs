9d7077e59c1b64fcc29f364c481a4305eb263eb1
olga <olga.levina.s@gmail.com>
2019-11-11 17:23:40 +0300

    Создан проект

--- SergeiLevin0/Startup.cs
+++ SergeiLevin0/Startup.cs
@@ -5,14 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SergeiLevin0
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Config) => Configuration = Config;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //набиваем сервисами, с которыми в дальнейшем будет работать наше приложение
         public void ConfigureServices(IServiceCollection services)
         {
         }
