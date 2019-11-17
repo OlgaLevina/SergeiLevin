ea856d772d580904d6aaa9a132c0dccc35c859c9
olga <olga.levina.s@gmail.com>
2019-11-17 21:42:58 +0300

    Рассмотрены разные варианта ППО

--- SergeiLevin0/Startup.cs
+++ SergeiLevin0/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SergeiLevin0.Infrastructure.Convenctions;
 
 namespace SergeiLevin0
 {
@@ -21,31 +22,47 @@ namespace SergeiLevin0
         //набиваем сервисами, с которыми в дальнейшем будет работать наше приложение
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc();//запуск mvc без внесения изменений в конвенции.
+            //services.AddMvc(opt => opt.Conventions.Add(new CustomControllerConvention())); //пример внесения изменений в конвенции на этапе старта - то что в скобках мое предположение.
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) //содержит промежуточное ПО, которое добавляется к нашему приложению через app.Use..., и формируют корвеер обработки входящих запросов в той послеовательности, как добавленяется.
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); //ПО занимающиеея отслеживанием ошибок - позволяет отселдить, что не так
                 app.UseBrowserLink();
             }
 
             //app.UseStaticFiles(new StaticFileOptions {ServeUnknownFileTypes = true});//будут подсоединяться любые файлы
             app.UseStaticFiles();//будут подсоединяться файлы стандартного содрежимого, т.е. картинки, html-страницы, css и т.п. 
             app.UseDefaultFiles();
+            app.UseCookiePolicy();//ППО для подстверждения политики куков, особенно в европе
+            //app.UseResponseCaching();//кэширование ответа
+            //app.UseResponseCompression();//ППО, которое работает после того, как отработал контроллер, берет сформированные данные и пытается их сжать, чтобы уменьшить объем данных на строне клиента
+            
+            //app.UseWelcomePage("/welcome");//пример промежуточного ПО
 
+            //app.Run(async context=>await context.Response.WriteAsync("Hello world"));//промежуточное ПО в виде терминального отдельного действия (т.е. является последним и дельнейшее в конвеере не выполняется). метод ран позволяет выполнить некоторое действие над контекстом входящего соединения и, например, отправить ответ
+
+            //app.Map("/Hello", applicattion=> applicattion.Run(async context => await context.Response.WriteAsync("Hello world")));// промежуточное дейстие в виде отдельного действия. Указываем путь и действие, действие к "новому" приложению, а не к app, чтобы не открывалось сразу. Таким образом можно создавать, например, адреса для технических нужд.
+
+            //можно прописать промежуточного ПО в виде отдельного класса - https://metanit.com/sharp/aspnet5/2.2.php
+
+            //app.UseAuthentication();//промежуточное ПО для авторизации. Будет отслеживать заголовки входящих сообщений и искать в них соотвествующие элементы
+
+            app.UseSession();//ППО для использования сессий в приложении
             //app.UseMvcWithDefaultRoute(); автоматическая конфигурация того, что ниже
-            app.UseMvc(routes =>
+            app.UseMvc(routes => // доступ к инфрастуктуре mvc - конфигурирует объект, который добавляется в конвеер, занимается распаковкой данных из запроса, запуском кнтроллеров представлений и отправкой ответов обратно
             {
                 routes.MapRoute(name:"default", template:"{controller=Home}/{action=Index}/{id?}"); //?-опциональный параметр, ! - обязательный параметр (как и ничего) , если параметр обязательный, то его отсуттвие в вводе пользователя приведет к ошибке
                 // для action  - можно ничего не устанавливать, либо установить значение по умолчанию, в данном случае Index
                 // аналогично для конроллера, можно утсановить значение по умолчанию Home, например, когда сервер обращается сам к себе
             }); // новый обработчик
             
-            // убираем прежный обработчик, т.к. существует и работает только первый
+            // убираем прежный обработчик, т.к. UseMvc является термиральным и дальше него обработка не идет
 
             //app.Run(async (context) =>
             //{
