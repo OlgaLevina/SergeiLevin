88c16a16b025bae73294f9d91fdf408d0bb68c8a
olga <olga.levina.s@gmail.com>
2019-11-30 13:26:38 +0300

    Добавлено представление регистрации

---
+++ SergeiLevin0/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SergeiLevin0.ViewModels.Idntity;
+
+namespace SergeiLevin0.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View(new RegisterUserViewModel());
+        public IActionResult Login() => View();
+
+    }
+}
