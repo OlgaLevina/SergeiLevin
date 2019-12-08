f2bd627b8169d0c03a51f4c0858796829e969567
olga <olga.levina.s@gmail.com>
2019-12-08 11:04:18 +0300

    Добавлены контроллер и представления для работы с товарами в админку

--- SergeiLevin0/Areas/Admin/Controllers/HomeController.cs
+++ SergeiLevin0/Areas/Admin/Controllers/HomeController.cs
@@ -5,15 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SergeiLevin0.Domain.Entities.Identity;
+using SergeiLevin0.Infrastructure.Interfaces;
 
 namespace SergeiLevin0.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles =Role.Administrator)]//соглашение для контроллеров внутри областей не прописаны, можно их объявить самостоятельно в  public void ConfigureServices - services.AddMvc(); (Startup), тогда уже не нужно будет писать этот атрибут 
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //private readonly IProductData ProductData;
+
+        //public HomeController(IProductData productData) => ProductData = productData;
+
+
+        public IActionResult Index()=>     View();
+        //public IActionResult List() => View(ProductData.GetProducts());//работа с продуктами вынесена в отдельный контроллекр
+        //public IActionResult Edit(int? id) => View();
+        //public IActionResult Delete(int? id) => View();
+        //[HttpPost, ActionName(nameof(Delete))]
+        //public IActionResult ProductDeleteConfirm(int? id) => RedirectToAction("List");
     }
 }
