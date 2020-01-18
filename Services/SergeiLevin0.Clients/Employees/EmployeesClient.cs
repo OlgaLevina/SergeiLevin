7f31d9f5a5364cc8565600567da36e5d83efdb2f
olga <olga.levina.s@gmail.com>
2020-01-18 19:52:43 +0300

    Базовый клиент дополнен методами

---
+++ Services/SergeiLevin0.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using SergeiLevin0.Clients.Base;
+using SergeiLevin0.Domain.ViewModels;
+using SergeiLevin0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SergeiLevin0.Clients.Employees
+{
+    public class EmployeesClient: BaseClient, IEmpoyeesData
+    {
+        private readonly IConfiguration configuration;
+
+        public EmployeesClient(IConfiguration configuration) : base(configuration, "api/employees") { }
+
+        public void Add(EmployeeView employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, EmployeeView employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeView> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeView GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
