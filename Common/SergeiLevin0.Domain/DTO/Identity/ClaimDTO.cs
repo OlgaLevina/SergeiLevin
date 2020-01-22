0f9080ffcb8c745ef04528bf29fe10331bdbe422
olga <olga.levina.s@gmail.com>
2020-01-22 11:56:34 +0300

    Добавлены дто-модели системы айдентити

---
+++ Common/SergeiLevin0.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace SergeiLevin0.Domain.DTO.Identity
+{
+    public abstract class ClaimDTO: UserDTO //модель для хранения утверждений, с которыми ассоциируется пользователь, например, что можно установить, что он администратор без ассоциации с ролью администратора. С помощью этого можно организовать регистрацию на сайте с помощью сторонних поставщиков регистрации
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+    public class AddClaimDTO : ClaimDTO { }//модель для добавления клэймов
+
+    public class RemoveClaimDTO : ClaimDTO { }
+
+    public class ReplaceClaimDTO : UserDTO
+    {
+        public Claim Claim { get; set; }
+        public Claim NewClaim { get; set; }
+    }
+
+}
---
+++ Common/SergeiLevin0.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SergeiLevin0.Domain.Entities.Identity;
+using System;
+using System.Security.Claims;
+using System.Text;
+
+namespace SergeiLevin0.Domain.DTO.Identity
+{
+    public abstract class UserDTO
+    {
+        public User User { get; set; }
+    }
+
+    public class AddLoginDTO: UserDTO //каждый раз, когда система айдентити регистрирует корректный вход в систему (ввели логин и пароль, а затем с помощью  юзерсайнменеджера залогинились в систему) данный класс отправляется на сторону вэбайпи и содержать информацию по юзеру (юзердто) и юзерлогининфо (структура, содержащая параметры вошедшего пользователя, в т.ч. провайдер)
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDTO: UserDTO //модель для хранения хешьсуммы
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO: UserDTO //для установления даты окончания блокировки; если она равна No - то блокировка бессрочная - используется для забанивания пользователя
+    {
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+
+}
