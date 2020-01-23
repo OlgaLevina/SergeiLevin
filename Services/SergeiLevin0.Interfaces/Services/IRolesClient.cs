208db27f4dd12cba031199eb70b5e8bc45547c87
olga <olga.levina.s@gmail.com>
2020-01-23 15:15:48 +0300

    Исправлена ошибка применения стандартного интерфейса в интерфэйсе ролей

--- Services/SergeiLevin0.Interfaces/Services/IRolesClient.cs
+++ Services/SergeiLevin0.Interfaces/Services/IRolesClient.cs
@@ -3,5 +3,5 @@ using SergeiLevin0.Domain.Entities.Identity;
 
 namespace SergeiLevin0.Interfaces.Services
 {
-    public interface IRolesClient: IRoleClaimStore<Role> { } //т.к. он наследует от одного стандартного, то можно было бы и не переопределять
+    public interface IRolesClient: IRoleStore<Role> { } //т.к. он наследует от одного стандартного, то можно было бы и не переопределять
 }
