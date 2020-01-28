0d47944d5fa4382c35f2e06b0de22ad47cfc183c
olga <olga.levina.s@gmail.com>
2020-01-28 19:39:06 +0300

    Добавлен ручной мэппинг

---
+++ Services/SergeiLevin0.Services/Map/CategoryMapper.cs
@@ -0,0 +1,20 @@
+﻿using SergeiLevin0.Domain.Entities;
+
+namespace SergeiLevin0.Services.Map
+{
+    public static class CategoryMapper
+    {
+        //public static CategoryDTO ToDTO(this Category category) => category is null ? null : new CategoryDTO
+        //{
+        //    Id = category.Id,
+        //    Price = category.Price,
+        //    Quantity = category.Quantity
+        //};
+        //public static Category FromDTO(this CategoryDTO category) => category is null ? null : new Category
+        //{
+        //    Id = category.Id,
+        //    Price = category.Price,
+        //    Quantity = category.Quantity
+        //};
+    }
+}
---
+++ Services/SergeiLevin0.Services/Map/ProductMapper.cs
@@ -0,0 +1,29 @@
+﻿using SergeiLevin0.Domain.DTO.Products;
+using SergeiLevin0.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SergeiLevin0.Services.Map
+{
+    static public class ProductMapper
+    {
+        public static ProductDTO ToDTO(this Product product) => product is null ? null : new ProductDTO {
+            Id = product.Id,
+            Name = product.Name,
+            ImageUrl=product.ImageUrl,
+            Brand=product.Brand.ToDTO(),
+            Order=product.Order,
+            Price=product.Price
+        };
+        public static Product FromDTO(this ProductDTO product) => product is null ? null : new Product {
+            Id = product.Id,
+            Name = product.Name,
+            ImageUrl = product.ImageUrl,
+            Brand = product.Brand.FromDTO(),
+            BrandId=product.Brand?.Id,
+            Order = product.Order,
+            Price = product.Price 
+        };
+    }
+}
