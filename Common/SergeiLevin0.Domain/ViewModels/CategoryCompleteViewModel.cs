b1a908190f6ca74552e3079fb8c42f6445f58628
olga <olga.levina.s@gmail.com>
2020-02-03 00:48:30 +0300

    Выделение активной секции в каталоге

---
+++ Common/SergeiLevin0.Domain/ViewModels/CategoryCompleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SergeiLevin0.Domain.ViewModels
+{
+    public class CategoryCompleteViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        public int? CurrentParentCategory { get; set; }
+
+        public int? CurrentCategoryId { get; set; }
+    }
+}
--- UI/SergeiLevin0/Components/CategoriesViewComponent.cs
+++ UI/SergeiLevin0/Components/CategoriesViewComponent.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SergeiLevin0.Domain.Entities;
 
 namespace SergeiLevin0.Components
 {
@@ -16,25 +17,44 @@ namespace SergeiLevin0.Components
         public CategoriesViewComponent(IProductData productData) { ProductData = productData; }
 
         //метод обнаруживается с помощью рефлексии внутри класса 
-        public IViewComponentResult Invoke() => View(GetCategories()); //синхронная реализация
-        //public async Task<IViewComponentResult> InvokeAsync() => View(); //асинхронная реализация - тип реалищации выбирается в зависимости от назначения компонента
+        //public IViewComponentResult Invoke() => View(GetCategories()); //синхронная реализация
+        public IViewComponentResult Invoke(string CategoryId)
+        {
+            var category_id = int.TryParse(CategoryId, out var id) ? id : (int?)null;
 
-        private IEnumerable<CategoryViewModel> GetCategories()
+            var categories = GetCategories(category_id, out var parent_category_id);
+
+            return View(new CategoryCompleteViewModel
+            {
+                Categories = categories,
+                CurrentCategoryId = category_id,
+                CurrentParentCategory = parent_category_id
+            });
+        }        //public async Task<IViewComponentResult> InvokeAsync() => View(); //асинхронная реализация - тип реалищации выбирается в зависимости от назначения компонента
+
+        private IEnumerable<CategoryViewModel> GetCategories(int? CategoryId, out int? ParentCategoryId)
         {
             var categories = ProductData.GetCategories();
-            var parent_categories = categories.Where(category => category.ParentId is null).ToArray();
-            var parent_categories_views=parent_categories.Select(parent_category => new CategoryViewModel
-            { Id = parent_category.Id, Name = parent_category.Name, Order = parent_category.Order }).ToList();
-            foreach (var parent_category_view in parent_categories_views)
+            ParentCategoryId = null;
+            //var parent_categories = categories.Where(category => category.ParentId is null).ToArray();
+            //var parent_categories_views=parent_categories.Select(parent_category => new CategoryViewModel
+            var parent_categories=categories.Where(category => category.ParentId is null)
+                .ToArray().Select(parent_category=> new CategoryViewModel
+                { Id = parent_category.Id, Name = parent_category.Name, Order = parent_category.Order }).ToList();
+            foreach (var parent_category in parent_categories)
             {
-                var childs = categories.Where(category => category.ParentId == parent_category_view.Id);
+                var childs = categories.Where(category => category.ParentId == parent_category.Id);
                 foreach (var child_category in childs)
-                    parent_category_view.ChildCategories.Add(new CategoryViewModel
-                    { Id = child_category.Id, Name = child_category.Name, Order = child_category.Order, ParentCategory=parent_category_view });
-                parent_category_view.ChildCategories.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+                {
+                    if (child_category.Id == CategoryId)
+                        ParentCategoryId = parent_category.Id;
+                    parent_category.ChildCategories.Add(new CategoryViewModel
+                    { Id = child_category.Id, Name = child_category.Name, Order = child_category.Order, ParentCategory = parent_category });
+                    parent_category.ChildCategories.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+                }
             }
-            parent_categories_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
-            return parent_categories_views;
+            parent_categories.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            return parent_categories;
         }
     }
 }
