ffeb0926565cf9502dbd6ea7597141aaf613a413
olga <olga.levina.s@gmail.com>
2020-02-03 01:29:22 +0300

    Макет и представление хлебных крошек

---
+++ Common/SergeiLevin0.Domain/ViewModels/BreadCrumbs/BreadCrumbViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SergeiLevin0.Domain.ViewModels.BreadCrumbs
+{
+    public class BreadCrumbViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public BreadCrumbsType BreadCrumbsType { get; set; }
+    }
+}
