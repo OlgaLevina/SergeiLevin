8da5219458abbf026333d60dbda693d30e22fd08
olga <olga.levina.s@gmail.com>
2020-02-06 16:15:59 +0300

    Добавлен визуальный компонент корзины в заголовке. Поправлены ссылки на добавление товара. vc:cart - выдает ошибку!!!!

---
+++ UI/SergeiLevin0/Components/CartViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SergeiLevin0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SergeiLevin0.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService CartService;
+        public CartViewComponent(ICartService cartService) => CartService = cartService;
+        public IViewComponentResult Invoke() => View(CartService.TransformFromCart());
+    }
+}
