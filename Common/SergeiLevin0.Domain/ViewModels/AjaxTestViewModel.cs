efa0f3dc1db39bb1cc31787904e04f0658620db4
olga <olga.levina.s@gmail.com>
2020-02-11 14:47:12 +0300

    Использование ненавящего ajax-а.

---
+++ Common/SergeiLevin0.Domain/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SergeiLevin0.Domain.ViewModels
+{
+   public class AjaxTestViewModel
+    {
+        public string Message { get; set; }
+        public int Id { get; set; }
+        public DateTime ServerTime { get; set; }
+    }
+}
---
+++ UI/SergeiLevin0/Controllers/AjaxTestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SergeiLevin0.Domain.ViewModels;
+
+namespace SergeiLevin0.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetJSON(int? id, string msg)
+        {
+            await Task.Delay(2000);
+            //возвращаем некие данные
+            return Json(new
+            {
+                Message = $"Response (id:{id ?? -1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        public async Task<IActionResult> GetTestView(int? id, string msg)
+        {
+            await Task.Delay(2000);
+            //возвращаем частичное предстваление с данными из тестовой модели
+            return PartialView("Partial/_DataView", new AjaxTestViewModel
+            {
+                Id = id ?? -1,
+                Message = msg ?? "<null>",
+                ServerTime = DateTime.Now
+            });
+        }
+    }
+}
